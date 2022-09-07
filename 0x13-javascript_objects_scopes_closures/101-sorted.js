0e6cb9d24eec870d4129c8a5027eb69857e51189
Sofiyullah Abdullah <66748323+Precioussoul@users.noreply.github.com>
2022-09-08 00:38:41 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,11 +2,10 @@
 const dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  }
-  else {
+if (newDict[dict[key]] === undefined) {
+  newDict[dict[key]] = [key];
+} else {
     newDict[dict[key]].push(key);
   }
-}
+};
 console.log(newDict);
